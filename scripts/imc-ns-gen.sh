96045e08cb956864693fc7a4257ac3dd86359669
quachconghoang <hoangqc@vnu.edu.vn>
2024-06-20 13:26:00 +1000

    Update images

--- scripts/imc-ns-gen.sh
+++ scripts/imc-ns-gen.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ns-process-data images --data ./images/ --output-dir ./ns/ --skip-colmap --colmap-model-path ./sfm/ --skip-image-processing
 ns-train splatfacto --data ./ns/
+
+
+ns-process-data images --data ./images/ --output-dir ./ns/ --camera-type simple_pinhole --feature-type superpoint --matcher-type superglue --no-use-single-camera-mode
